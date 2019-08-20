2bcfc15a49fa84eb6fcf41023773d7494b33cf79
NatureGeorge <414731811@qq.com>
2019-08-21 00:18:45 +0800

    Update Via PC 0821

--- src/py_src/MMCIFplus_unit.py
+++ src/py_src/MMCIFplus_unit.py
@@ -1,7 +1,7 @@
 # @Date:   2019-08-19T19:29:29+08:00
 # @Email:  1730416009@stu.suda.edu.cn
 # @Filename: MMCIFplus_unit.py
-# @Last modified time: 2019-08-20T21:15:16+08:00
+# @Last modified time: 2019-08-21T00:16:09+08:00
 from collections import defaultdict
 import pandas as pd
 import numpy as np
@@ -22,6 +22,19 @@ class MMCIF_unit(Unit):
             '/data1/suntt/CanDriver/Data/PDB_NEW/mmcif_file/'
             ],
         'MMCIF_FOLDER': '../../data/Mapping_Pipeline/mmcif_file/',
+        'COMMON_COL': ['_pdbx_audit_revision_history.revision_date', '_exptl.method', '_em_3d_reconstruction.resolution', '_refine.ls_d_res_high'],
+        'ENTITY_COL': ['_entity.pdbx_mutation', '_entity.id'],
+        'TYPE_COL':['_entity_poly.entity_id', '_entity_poly.pdbx_strand_id', '_entity_poly.type'],
+        'SEQRES_COL':['_pdbx_poly_seq_scheme.pdb_strand_id',
+                 '_pdbx_poly_seq_scheme.mon_id','_pdbx_poly_seq_scheme.pdb_mon_id', '_pdbx_poly_seq_scheme.auth_mon_id',
+                 '_pdbx_poly_seq_scheme.ndb_seq_num', '_pdbx_poly_seq_scheme.pdb_seq_num',
+                 '_pdbx_poly_seq_scheme.auth_seq_num', '_pdbx_poly_seq_scheme.pdb_ins_code'],
+        'LIGAND_COL': [
+                 '_struct_conn.ptnr2_auth_asym_id','_struct_conn.ptnr2_auth_comp_id',
+                 '_struct_conn.ptnr2_auth_seq_id',
+                 '_struct_conn.conn_type_id',
+                 '_struct_conn.ptnr1_auth_asym_id', '_struct_conn.ptnr1_auth_comp_id',
+                 '_struct_conn.ptnr1_auth_seq_id'],
         'LIGAND_LIST': [
                         'ZN', 'MG', 'CA', 'FE', 'NA', 'MN', 'K', 'NI', 'CU', 'CO', 'CD', 'HG', 'PT', 'MO', 'BE', 'AL', 'BA',
                         'RU', 'SR', 'V', 'CS', 'W', 'AU', 'YB', 'LI', 'GD', 'PB', 'Y', 'TL', 'IR', 'RB', 'SM', 'AG',
@@ -83,8 +96,7 @@ class MMCIF_unit(Unit):
                 else:
                     info_dict[key].append(data)
 
-    def get_date_muta_resolution_method(self, path_list):
-        # 'C:\\Users\\Nature\\Desktop\\LiGroup\\Filter_new_20190123\\doc_in\\'
+    def get_data_from_mmcif(self, path_list):
         '''
         {
             '_pdbx_audit_revision_history.revision_date': ['initial_version_time', 'newest_version_time'], # sometimes not a list
@@ -101,45 +113,28 @@ class MMCIF_unit(Unit):
                 print(path)
                 info_dict['pdb_id'].append(path[-8:-4])
                 MMCIF_unit.get_mmcif_info(
-                    [
-                    '_pdbx_audit_revision_history.revision_date',
-                    '_entity.pdbx_mutation',
-                    '_entity.id',
-                    '_exptl.method',
-                    '_em_3d_reconstruction.resolution',
-                    '_refine.ls_d_res_high',
-                    '_entity_poly.entity_id',
-                    '_entity_poly.pdbx_strand_id',
-                    '_entity_poly.type',
-                    # seqres_atom
-                    '_pdbx_poly_seq_scheme.mon_id', '_pdbx_poly_seq_scheme.ndb_seq_num',
-                             '_pdbx_poly_seq_scheme.pdb_seq_num', '_pdbx_poly_seq_scheme.auth_seq_num',
-                             '_pdbx_poly_seq_scheme.pdb_mon_id', '_pdbx_poly_seq_scheme.auth_mon_id',
-                             '_pdbx_poly_seq_scheme.pdb_strand_id', '_pdbx_poly_seq_scheme.pdb_ins_code',
-                    # ligand
-                    '_struct_conn.conn_type_id','_struct_conn.ptnr1_auth_asym_id',
-                             '_struct_conn.ptnr1_auth_comp_id','_struct_conn.ptnr1_auth_seq_id',
-                             '_struct_conn.ptnr2_auth_asym_id','_struct_conn.ptnr2_auth_comp_id',
-                             '_struct_conn.ptnr2_auth_seq_id'
-                    ],
-                    [
-                    '_exptl.method',
-                    '_em_3d_reconstruction.resolution',
-                    '_refine.ls_d_res_high'],
+                    MMCIF_unit.CONFIG['COMMON_COL'] + \
+                    MMCIF_unit.CONFIG['ENTITY_COL'] + \
+                    MMCIF_unit.CONFIG['TYPE_COL'] + \
+                    MMCIF_unit.CONFIG['SEQRES_COL'] + \
+                    MMCIF_unit.CONFIG['LIGAND_COL'],
+                    MMCIF_unit.CONFIG['COMMON_COL'][1:],
                     info_dict,
                     path)
 
+        # Deal with Residues in SEQRES_COL
+        resides_col_li = MMCIF_unit.CONFIG['SEQRES_COL'][1:4]
         mtoTool = Unit.MultiToOne()
-        for i in range(len(info_dict['_pdbx_poly_seq_scheme.mon_id'])):
-            info_dict['_pdbx_poly_seq_scheme.mon_id'][i] = ''.join([mtoTool.multi_letter_convert_to_one_letter(j) for j in info_dict['_pdbx_poly_seq_scheme.mon_id'][i]])
-            info_dict['_pdbx_poly_seq_scheme.auth_mon_id'][i] = ''.join([mtoTool.multi_letter_convert_to_one_letter(j) for j in info_dict['_pdbx_poly_seq_scheme.auth_mon_id'][i]])
-            info_dict['_pdbx_poly_seq_scheme.pdb_mon_id'][i] = ''.join([mtoTool.multi_letter_convert_to_one_letter(j) for j in info_dict['_pdbx_poly_seq_scheme.pdb_mon_id'][i]])
-
+        for i in range(len(resides_col_li[0])):
+            for resides_col in resides_col_li:
+                info_dict[resides_col][i] = ''.join([mtoTool.multi_letter_convert_to_one_letter(j) for j in info_dict[resides_col][i]])
 
         get_index = lambda x, y, z: y[x[z]:x[z+1]] if len(x) != 1 and z+1 < len(x) else y[x[z]:]
-        for i in range(len(info_dict['_pdbx_poly_seq_scheme.pdb_strand_id'])):
+        # Deal with SEQRES_COL
+        pdbx_poly_key = MMCIF_unit.CONFIG['SEQRES_COL'][0]
+        for i in range(len(info_dict[pdbx_poly_key])):
             strand_id_index = [0]
-            li = info_dict['_pdbx_poly_seq_scheme.pdb_strand_id'][i]
+            li = info_dict[pdbx_poly_key][i]
             save_id = li[0]
             strand_id_li = [save_id]
             for j in range(len(li)):
@@ -147,7 +142,7 @@ class MMCIF_unit(Unit):
                     save_id = li[j]
                     strand_id_index.append(j)
                     strand_id_li.append(save_id)
-            info_dict['_pdbx_poly_seq_scheme.pdb_strand_id'][i] = strand_id_li
+            info_dict[pdbx_poly_key][i] = strand_id_li
 
             info_dict['_pdbx_poly_seq_scheme.mon_id'][i] = [
                 get_index(strand_id_index, info_dict['_pdbx_poly_seq_scheme.mon_id'][i], j)
@@ -176,28 +171,22 @@ class MMCIF_unit(Unit):
             info_dict['_pdbx_poly_seq_scheme.pdb_ins_code'][i] = [';'.join(
                 get_index(strand_id_index, info_dict['_pdbx_poly_seq_scheme.pdb_ins_code'][i], j))
                 for j in range(len(strand_id_index))]
-
-        ligand_col_list = ['_struct_conn.ptnr1_auth_asym_id',
-                 '_struct_conn.ptnr1_auth_comp_id','_struct_conn.ptnr1_auth_seq_id',
-                 '_struct_conn.ptnr2_auth_asym_id','_struct_conn.ptnr2_auth_comp_id',
-                 '_struct_conn.ptnr2_auth_seq_id','_struct_conn.conn_type_id']
-        
-        ligand_col_tp = tuple(info_dict[i] for i in ligand_col_list)
-        ligand_col_zip_li = list(zip(*ligand_col_tp))
-        ligand_col_zip_li.sort()
-        # info_dict[i] = zip(*ligand_col_zip_li)
-        print(ligand_col_zip_li)
-        for i in range(len(ligand_col_list)):
-            info_dict[ligand_col_list[i]] = [j[i] for j in ligand_col_zip_li]
-
-
-
-        for i in range(len(info_dict['_struct_conn.ptnr1_auth_asym_id'])):
+        # Deal with LIGAND_COL: Sort the data
+        ligand_col_list = MMCIF_unit.CONFIG['LIGAND_COL']
+        for i in range(len(info_dict[ligand_col_list[0]])):
+            ligand_col_tp = tuple(info_dict[col][i] for col in ligand_col_list)
+            ligand_col_zip_li = list(zip(*ligand_col_tp))
+            ligand_col_zip_li.sort()
+            for col_index in range(len(ligand_col_list)):
+                info_dict[ligand_col_list[col_index]][i] = [tp[col_index] for tp in ligand_col_zip_li]
+        # Deal with LIGAND_COL: Group the data
+        ligand_group_col = ligand_col_list[0]
+        for i in range(len(info_dict[ligand_group_col])):
             strand_id_index = [0]
-            li = info_dict['_struct_conn.ptnr1_auth_asym_id'][i]
+            li = info_dict[ligand_group_col][i]
             if not li:
-                info_dict['_struct_conn.ptnr1_auth_asym_id_index'].append([])
-                info_dict['_struct_conn.ptnr1_auth_asym_id_li'].append([])
+                info_dict['%s_index'%ligand_group_col].append([])
+                info_dict['%s_li'%ligand_group_col].append([])
                 continue
             save_id = li[0]
             strand_id_li = [save_id]
@@ -206,47 +195,24 @@ class MMCIF_unit(Unit):
                     save_id = li[j]
                     strand_id_index.append(j)
                     strand_id_li.append(save_id)
-            info_dict['_struct_conn.ptnr1_auth_asym_id_li'].append(strand_id_li)
-            info_dict['_struct_conn.ptnr1_auth_asym_id_index'].append(strand_id_index)
+            info_dict['%s_li'%ligand_group_col].append(strand_id_li)
+            info_dict['%s_index'%ligand_group_col].append(strand_id_index)
 
-            info_dict['_struct_conn.conn_type_id'][i] = [
-                get_index(strand_id_index, info_dict['_struct_conn.conn_type_id'][i], j)
-                for j in range(len(strand_id_index))]
-
-            info_dict['_struct_conn.ptnr1_auth_comp_id'][i] = [
-                get_index(strand_id_index, info_dict['_struct_conn.ptnr1_auth_comp_id'][i], j)
-                for j in range(len(strand_id_index))]
-
-            info_dict['_struct_conn.ptnr1_auth_seq_id'][i] = [
-                get_index(strand_id_index, info_dict['_struct_conn.ptnr1_auth_seq_id'][i], j)
-                for j in range(len(strand_id_index))]
-
-            info_dict['_struct_conn.ptnr1_auth_asym_id'][i] = [
-                get_index(strand_id_index, info_dict['_struct_conn.ptnr1_auth_asym_id'][i], j)
-                for j in range(len(strand_id_index))]
-
-            info_dict['_struct_conn.ptnr2_auth_asym_id'][i] = [
-                get_index(strand_id_index, info_dict['_struct_conn.ptnr2_auth_asym_id'][i], j)
-                for j in range(len(strand_id_index))]
-
-            info_dict['_struct_conn.ptnr2_auth_comp_id'][i] = [
-                get_index(strand_id_index, info_dict['_struct_conn.ptnr2_auth_comp_id'][i], j)
-                for j in range(len(strand_id_index))]
-
-            info_dict['_struct_conn.ptnr2_auth_seq_id'][i] = [
-                get_index(strand_id_index, info_dict['_struct_conn.ptnr2_auth_seq_id'][i], j)
-                for j in range(len(strand_id_index))]
-
+            for col in ligand_col_list:
+                info_dict[col][i] = [
+                    get_index(strand_id_index, info_dict[col][i], j)
+                    for j in range(len(strand_id_index))]
 
         df = pd.DataFrame(info_dict)
+        # Deal with the date of structure
         df['initial_version_time'] = df.apply(lambda x: x['_pdbx_audit_revision_history.revision_date'][0], axis=1)
         df['newest_version_time'] = df.apply(lambda x: x['_pdbx_audit_revision_history.revision_date'][-1], axis=1)
-
+        # Deal with the mutations
         muta_count = lambda x: x.count(',')+1 if x!= '?' else 0
         df['mutation_num'] = df.apply(lambda x: [muta_count(i) for i in x['_entity.pdbx_mutation']], axis=1)
-
+        # Deal with the resolution
         df['resolution'] = df.apply(lambda x: x['_refine.ls_d_res_high'] if x['_exptl.method']=='X-RAY DIFFRACTION' else x['_em_3d_reconstruction.resolution'], axis=1)
-
+        # Change the columns
         df.rename(columns={'_exptl.method':'method'},inplace=True)
         df.drop(columns=['_pdbx_audit_revision_history.revision_date','_em_3d_reconstruction.resolution','_refine.ls_d_res_high'],inplace=True)
 
@@ -259,6 +225,6 @@ if __name__ == '__main__':
     file_list = os.listdir(route)
     file_p_list = [route+i for i in file_list]
     mmcif_demo = MMCIF_unit()
-    df = mmcif_demo.get_date_muta_resolution_method(file_p_list)
+    df = mmcif_demo.get_data_from_mmcif(file_p_list)
     for i in df.index:
         print(df.loc[i,])
