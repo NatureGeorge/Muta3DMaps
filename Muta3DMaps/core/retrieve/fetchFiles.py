9348dade6e1d68bbee5d3ac6cc20f142fdd274dd
NatureGeorge <414731811@qq.com>
2020-02-09 22:43:54 +0800

    Implement both GET and POST & aiohttp.StreamReader

--- Muta3DMaps/core/retrieve/fetchFiles.py
+++ Muta3DMaps/core/retrieve/fetchFiles.py
@@ -2,7 +2,7 @@
 # @Filename: fetchFiles.py
 # @Email:  1730416009@stu.suda.edu.cn
 # @Author: ZeFeng Zhu
-# @Last Modified: 2020-02-08 04:15:07 pm
+# @Last Modified: 2020-02-09 08:50:23 pm
 # @Copyright (c) 2020 MinghuiGroup, Soochow University
 import os
 from time import perf_counter
@@ -13,15 +13,31 @@ from unsync import unsync
 from tenacity import retry, wait_random_exponential, stop_after_attempt, after_log, RetryError
 import logging
 from tqdm import tqdm
-from typing import Iterable, Iterator, Union
+from typing import Iterable, Iterator, Union, Any, Optional, List, Dict
 
 
 class UnsyncFetch(object):
+    '''
+    Fetch files through API in unsync/async way
+
+    Note:
+
+    * Implement both GET and POST method
+    * Since the methods in this class would not load all the response data in memory,
+      the response data could be a large file
+    * Parameters of `tenacity.retry` is built-in
+    * Reference: https://docs.aiohttp.org/en/stable/client_quickstart.html
+    '''
 
     logger = logging.getLogger("UnsyncFetch"); logger.setLevel(logging.DEBUG)
     streamHandler = logging.StreamHandler(); streamHandler.setLevel(logging.INFO)
     formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s"); streamHandler.setFormatter(formatter)
     logger.addHandler(streamHandler)
+    
+    retry_kwargs = {
+        'wait': wait_random_exponential(multiplier=1, max=15),
+        'stop': stop_after_attempt(3),
+        'after': after_log(logger, logging.WARNING)}
 
     @classmethod
     def set_logging_fileHandler(cls, path: str, level: int = logging.DEBUG, formatter=formatter):
@@ -30,56 +46,75 @@ class UnsyncFetch(object):
             fileHandler.setLevel(level)
             fileHandler.setFormatter(formatter)
             cls.logger.addHandler(fileHandler)
-            cls.logger.info(f"Logging file has been created in {path}")
+            cls.logger.info(f"Logging file in {path}")
         except Exception:
             cls.logger.warning("Invalid file path for logging file ! Please specifiy path=...")
 
     @classmethod
-    @retry(wait=wait_random_exponential(multiplier=1, max=15), stop=stop_after_attempt(3), after=after_log(logger, logging.WARNING))
-    async def download_file(cls, url: str, post_data=Union[str, None]):
-        cls.logger.debug(f"Start to get file: {url}")
+    @retry(**retry_kwargs)
+    async def download_file(cls, method: str, info: Dict, path: str, chunk_size: int = 1024*1024):
+        cls.logger.debug(f"Start to download file: {info}")
         async with aiohttp.ClientSession() as session:
-            if post_data is None:
-                async_func = session.get
-                kwargs = {'url': url}
-            else:
-                async_func = session.post
-                kwargs = {'url': url, 'data': post_data}
-
-            async with async_func(**kwargs) as resp:
+            async_func = getattr(session, method)
+            async with async_func(**info) as resp:
                 if resp.status == 200:
-                    return await resp.read()
-                elif resp.status == 404:
+                    chunk = await resp.content.read(chunk_size)
+                    with open(path, 'wb') as fileOb:
+                        while chunk:
+                            fileOb.write(chunk)
+                            chunk = await resp.content.read(chunk_size)
+                    cls.logger.debug(f"File has been saved in: {path}")
+                    return path
+                elif resp.status in (404, 405):
+                    cls.logger.warning(f"404/405 for: {info}")
                     return None
                 else:
-                    mes = "code={resp.status}, message={resp.reason}, headers={resp.headers}"
-                    raise Exception(mes.format(resp=resp))
+                    mes = "code={resp.status}, message={resp.reason}, headers={resp.headers}".format(resp=resp)
+                    cls.logger.error(mes)
+                    raise Exception(mes)
 
     @classmethod
     @unsync
     async def save_file(cls, path: str, data: bytes):
+        '''Deprecated'''
         cls.logger.debug(f"Start to save file: {path}")
-        async with aiofiles.open(path, 'wb') as jsonFile:
-            await jsonFile.write(data)
+        async with aiofiles.open(path, 'wb') as fileOb:
+            await fileOb.write(data)
 
     @classmethod
     @unsync
-    async def fetch_file(cls, semaphore: asyncio.Semaphore, url: str, post_data: Union[str, None], path: str, rate: float):
-        async with semaphore:
-            try:
-                data = await cls.download_file(url, post_data)
-                if data is not None:
-                    await cls.save_file(path, data)
+    async def fetch_file(cls, semaphore: asyncio.Semaphore, method: str, info: Dict, path: str, rate: float) -> Optional[str]:
+        try:
+            async with semaphore:
+                res = await cls.download_file(method, info, path)
+                if res is not None:
                     await asyncio.sleep(rate)
-                    return path
-                else:
-                    cls.logger.warning(f"404 for: {url}")
-            except RetryError:
-                cls.logger.error(f"Retry failed for: {url}")
+                return res
+        except RetryError:
+            cls.logger.error(f"Retry failed for: {info}")
+    '''
+    @classmethod
+    @unsync
+    async def fetch_file(cls, semaphore: asyncio.Semaphore, method: str, info: Dict, path: str, rate: float) -> Optional[str]:
+        try:
+            async with semaphore:
+                async with aiofiles.open(path, 'wb') as fileOb:
+                    async for chunk in cls.download_file(method, info):
+                        if chunk is None:
+                            cls.logger.warning(f"404/405 for: {info}")
+                            return None
+                        else:
+                            await fileOb.write(chunk)
+                cls.logger.debug(f"File has been saved in: {path}")
+                await asyncio.sleep(rate)
+                return path
+        except RetryError:
+            cls.logger.error(f"Retry failed for: {info}")
+    '''
 
     @classmethod
     @unsync
-    async def multi_tasks(cls, workdir: str, tasks: Union[Iterable, Iterator], concur_req: int = 4, rate: float = 1.5):
+    async def multi_tasks(cls, workdir: str, tasks: Union[Iterable, Iterator], concur_req: int = 4, rate: float = 1.5) -> List[Optional[str]]:
         '''
         Template for multiTasking
 
@@ -88,8 +123,8 @@ class UnsyncFetch(object):
             2. unit func
         '''
         semaphore = asyncio.Semaphore(concur_req)
-        # await asyncio.gather(*[cls.fetch_file(semaphore, url, post_data, os.path.join(workdir, path), rate) for url, post_data, path in tasks])
-        tasks = [cls.fetch_file(semaphore, url, post_data, os.path.join(workdir, path), rate) for url, post_data, path in tasks]
+        # await asyncio.gather(*[cls.fetch_file(semaphore, method, info, os.path.join(workdir, path), rate) for method, info, path in tasks])
+        tasks = [cls.fetch_file(semaphore, method, info, os.path.join(workdir, path), rate) for method, info, path in tasks]
         return [await fob for fob in tqdm(asyncio.as_completed(tasks), total=len(tasks))]
 
     @classmethod
@@ -102,4 +137,4 @@ class UnsyncFetch(object):
         return res
 
     # TODO: collect the data immediately as the file has been saved
-    # TODO: collect the data immediately as the target files in a group has all been saved
+    # TODO: collect the data immediately as the target files in a group has all been saved (Maybe not a good idea?)
--- Muta3DMaps/test/test_UnsyncFetch.py
+++ Muta3DMaps/test/test_UnsyncFetch.py
@@ -1,42 +1,29 @@
 import pytest
 from Muta3DMaps.core.retrieve.fetchFiles import UnsyncFetch
 
-DEMO = [
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/molecules/1a01',
-     None, '1a01_molecules.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/molecules/2xyn',
-     None, '2xyn_molecules.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/molecules/1miu',
-     None, '1miu_molecules.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/molecules/2hev',
-     None, '2hev_molecules.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/molecules/3g96',
-     None, '3g96_molecules.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/molecules/6lu7',
-     None, '6lu7_molecules.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/secondary_structure/',
-     '1a01,2xyn,2hev', '1a01,2xyn,2hev_secondary_structure.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/residue_listing/1a01',
-     None, '1a01_residue_listing.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/residue_listing/2xyn',
-     None, '2xyn_residue_listing.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/residue_listing/1miu',
-     None, '1miu_residue_listing.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/residue_listing/2hev',
-     None, '2hev_residue_listing.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pppp',
-     None, 'pppp_pppp.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/residue_listing/3g96',
-     None, '3g96_residue_listing.json'),
-    ('https://www.ebi.ac.uk/pdbe/api/pdb/entry/residue_listing/6lu7',
-     None, '6lu7_residue_listing.json')]
+
+BASE_URL = 'https://www.ebi.ac.uk/pdbe/api/pdb/entry'
+
+TASKS = [
+    ('get', {'url': f'{BASE_URL}/molecules/', 'params': '1a01'}, '1a01_molecules.json'), # 405
+    ('get', {'url': f'{BASE_URL}/molecules/1a01'}, '1a01_molecules.json'),
+    ('get', {'url': f'{BASE_URL}/molecules/2xyn'}, '2xyn_molecules.json'),
+    ('get', {'url': f'{BASE_URL}/molecules/1miu'}, '1miu_molecules.json'),
+    ('get', {'url': f'{BASE_URL}/molecules/2hev'}, '2hev_molecules.json'),
+    ('get', {'url': f'{BASE_URL}/molecules/3g96'}, '3g96_molecules.json'),
+    ('get', {'url': f'{BASE_URL}/molecules/6lu7'}, '6lu7_molecules.json'),
+    ('post', {'url': f'{BASE_URL}/summary/', 'data': '6lu7,3g96,2hev'}, '6lu7,3g96,2hev_summary.json'),
+    ('post', {'url': f'{BASE_URL}/summary/', 'data': '1a01,2xyn,1miu'}, '1a01,2xyn,1miu_summary.json'),
+    ('get', {'url': f'{BASE_URL}/residue_listing/1a01'}, '1a01_residue_listing.json'),
+    ('post', {'url': f'{BASE_URL}/status/', 'data': '1a01,pppp,2xyn'}, '1a01,pppp,2xyn_status.json') # contail 404, the server would ignore pppp
+]
 
 def test_main():
     try:
-        return UnsyncFetch.main(r'./data/', DEMO, 8)
+        return UnsyncFetch.main(r'./data/', TASKS, 8)
     except Exception:
         return -1
 
 def answer_main():
     res = test_main()
-    assert res == len(DEMO) and len([i for i in res if i is None]) == 2
+    assert res == len(TASKS)  # and len([i for i in res if i is None]) == 2
